375279dddde0a3b023627a90df311c45cdbc42da
Mishig Davaadorj <dmishig@gmail.com>
2024-05-08 09:35:13 +0200

    wip

--- prompts.js
+++ prompts.js
@@ -1,69 +1,125 @@
 export const prompts = [
   {
-    question: 'What were the news yesterday?',
-    answer: 'Should be a good summary of world news',
+    question: 'When is the next apple event?',
+    answer: 'June: WWDC (Worldwide Developers Conference)',
   },
   {
-    question: 'Who won last US open?',
-    answer: 'As of May 2, 2024, Djokovic and Coco Gauff',
+    question: 'Who is Victor Mustar?',
+    answer: 'Head of Product design at Hugging Face',
   },
-  // {
-  //   question: 'When is the next formula 1 grand prix?',
-  //   answer: 'As of May 2, 2024, it will be May 5 Mimai Grand Prix',
-  // },
-  // { question: 'Who is Julien Chaumond?', answer: 'CTO of hugging face' },
-  // {
-  //   question: 'Where is the hugging face HQ?',
-  //   answer: '20 Jay Street, Ste 620, 11201 Brooklyn USA',
-  // },
-  // {
-  //   question: "What's the title of Elon Musk's new biography?",
-  //   answer:
-  //     "just 'Elon Musk'. Not to be confused with the previous one titled 'Elon Musk: Tesla, SpaceX, and the Quest for Fantastic Future'",
-  // },
-  // {
-  //   question: "What's the highest point in Colorado?",
-  //   answer: 'At 14,440 ft., Mount Elbert',
-  // },
-  // {
-  //   question: 'What are good ski resorts in France?',
-  //   answer: 'Need to mention Chamonix at least',
-  // },
-  // {
-  //   question: 'Where is MC Escher museum?',
-  //   answer: 'The Hague or Den Haag, Netherlands',
-  // },
-  // {
-  //   question: 'What is diffusers AI?',
-  //   answer: 'It is a hugging face library for working with diffusion models',
-  // },
-  // {
-  //   question: 'What is spiral AI artwork?',
-  //   answer: 'AI art genre that encodes spiral patterns in images',
-  // },
-  // {
-  //   question: 'Who won the last fight between Shevchenko and Grasso?',
-  //   answer: 'It was a majority draw',
-  // },
-  // {
-  //   question: 'Who are the current UFC champions in every division?',
-  //   answer: '',
-  // },
-  // {
-  //   question: 'What is the print width of the Prestige A4 DTF printer?',
-  //   answer: '"7.87" or A4 size',
-  // },
-  // {
-  //   question: 'Does the DigiRIP RIP Software only run on windows?',
-  //   answer: 'yes, windows only',
-  // },
-  // {
-  //   question: 'What type of printhead does the Epson F2270 DTG printer use?',
-  //   answer:
-  //     'PrecisionCore MicroTFP printhead with Nozzle Verification Technology',
-  // },
-  // {
-  //   question: 'Which team is currently leading LaLiga?',
-  //   answer: 'As of May 2, 2024, it is Real Madrid',
-  // },
+  {
+    question: 'When is the next release date of the Iphone?',
+    answer: 'September 2024',
+  },
+  {
+    question: 'How to use 3d models in Gradio?',
+    answer: 'gr.Model3D(),',
+  },
+  {
+    question: 'How to do two-ways props binding in svelte?',
+    answer: 'bind:bindValue',
+  },
+  {
+    question: 'What are the new features incoming in Svelte 5?',
+    answer: `runes, (the new signal-powered reactivity API)
+    event handling with less boilerplate
+    better component composition
+    native TypeScript support`,
+  },
+  {
+    question: 'What famous rap artists have clashed recently?',
+    answer: 'Drake, kendrick ',
+  },
+  {
+    question: 'Which country recently elected a new president and who is it?',
+    answer: 'Bassirou Diomaye Faye, senegal',
+  },
+  {
+    question: 'What is the current price of Bitcoin?',
+    answer: '63 522,80USD',
+  },
+  {
+    question:
+      'Has the Federal Reserve announced any changes to interest rates?',
+    answer: 'Federal Reserve issues FOMC statement',
+  },
+  {
+    question: 'What was the most profitable movie in 2024?',
+    answer: 'Dune II (?)',
+  },
+  {
+    question: 'Can I set a background color for my kaboom.js game?',
+    answer: 'setBackground(color: Color, alpha?: number)',
+  },
+  {
+    question:
+      'Is there a Hugging Face application to generate Illusion images?',
+    answer: 'IllusionDiffusion',
+  },
+  {
+    question: 'Is there a way to use Transformers in Javascript?',
+    answer: 'Transformers.js',
+  },
+  {
+    question: 'What is Hugging Face evaluation library?',
+    answer: 'Evaluate',
+  },
+  {
+    question:
+      'How do I center an element horiontally and vertically using tailwindcss?',
+    answer: 'flex items-center justify-center or grid place-items-center',
+  },
+  {
+    question: 'What are the new features in tailwind 4.0?',
+    answer: 'New engine in Rust, unified toolchain, ...',
+  },
+  {
+    question: 'What is midjourney cheapest plan?',
+    answer: '$10/month',
+  },
+  { question: 'What does Jubiwee do?', answer: 'HR surveys' },
+  { question: 'Is LM Studio available for Linux?', answer: 'Yes' },
+  {
+    question:
+      "Quel est le créateur de contenu avec le plus d'abonnés sur le youtube francais?",
+    answer: 'Squeezie',
+  },
+  {
+    question: 'Quel président important a visité la france ce derniers jours?',
+    answer: 'Xi Jinping',
+  },
+  {
+    question: "Quel est le prix moyen d'un paquet de cigarette en France?",
+    answer: '11,50 euros',
+  },
+  {
+    question: 'When is the Ping Pong Olympics game tournament?',
+    answer: '27 July to 10 August',
+  },
+  {
+    question: 'When will the Opening Ceremony of the Olympic Games take place?',
+    answer: 'July 26, 2024',
+  },
+  {
+    question: 'Is HuggingChat available on iOS as a native app?',
+    answer: 'Yes',
+  },
+  {
+    question: 'Is HuggingChat available on Android as a native app?',
+    answer: 'No',
+  },
+  { question: 'What is LPCAMM2?', answer: 'Laptop memory' },
+  {
+    question: 'What is tesla model 3 price?',
+    answer: 'starts at $38,990',
+  },
+  {
+    question:
+      'What is the goal of the research paper StoryDiffusion: Consistent Self-Attention?',
+    answer: 'Maintain consistency across a series of generated images',
+  },
+  {
+    question: 'List 5 people working at Hugging Face?',
+    answer: 'the co-founders + 2 other',
+  },
 ]
